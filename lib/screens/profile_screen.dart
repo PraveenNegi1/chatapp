a7206acffd1090d73d072951fdeadf97e18e2f84
praveen <praveennegi700@gmail.com>
2026-02-28 11:01:53 +0530

    done

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -2,7 +2,6 @@ import 'package:chatapp/screens/login_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
